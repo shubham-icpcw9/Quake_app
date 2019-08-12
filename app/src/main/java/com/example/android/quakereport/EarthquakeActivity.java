9d88bde1819c30bde7226a2f15e16b284c38b9e1
Patawarika <shubham.patwarika@gmail.com>
2019-08-13 00:31:57 +0530

    commit1

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -18,11 +18,15 @@ package com.example.android.quakereport;
 import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -35,42 +39,25 @@ import android.widget.TextView;
 
 public class EarthquakeActivity extends AppCompatActivity implements LoaderCallbacks<List<Location>> {
 
-    private static final String USGS_REQUEST_URL =
-            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake" +
-                    "&orderby=time&minmag=3&limit=20";
+    //private static final String USGS_REQUEST_URL =
+           // "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake" +
+             //       "&orderby=time&minmag=3&limit=20";
+
+    private static final String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query";
+
     private static final int EARTHQUAKE_LOADER_ID = 1;
 
     private TextView mEmptyStateTextView;
 
     private EarthquakeAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
-        mEmptyStateTextView = (TextView) findViewById(R.id.empty_view);
+        mEmptyStateTextView = findViewById(R.id.empty_view);
         ListView earthquakeListView = findViewById(R.id.list);
         earthquakeListView.setEmptyView(mEmptyStateTextView);
-        adapter = new EarthquakeAdapter(getApplicationContext(), new LinkedList<Location>());
-        earthquakeListView.setAdapter(adapter);
-
-        earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // Find the current earthquake that was clicked on
-                Location currentEarthquake = adapter.getItem(position);
-
-                // Convert the String URL into a URI object (to pass into the Intent constructor)
-                Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
-
-                // Create a new intent to view the earthquake URI
-                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
-
-                // Send the intent to launch a new activity
-                startActivity(websiteIntent);
-            }
-        });
-
         // Get a reference to the ConnectivityManager to check state of network connectivity
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -80,6 +67,26 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
 
         // If there is a network connection, fetch data
         if (networkInfo != null && networkInfo.isConnected()) {
+
+            adapter = new EarthquakeAdapter(getApplicationContext(), new LinkedList<Location>());
+            earthquakeListView.setAdapter(adapter);
+
+            earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                    // Find the current earthquake that was clicked on
+                    Location currentEarthquake = adapter.getItem(position);
+
+                    // Convert the String URL into a URI object (to pass into the Intent constructor)
+                    Uri earthquakeUri = Uri.parse(currentEarthquake.getUrl());
+
+                    // Create a new intent to view the earthquake URI
+                    Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+
+                    // Send the intent to launch a new activity
+                    startActivity(websiteIntent);
+                }
+            });
             // Get a reference to the LoaderManager, in order to interact with loaders.
             LoaderManager loaderManager = getLoaderManager();
 
@@ -87,7 +94,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
             // the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
             // because this activity implements the LoaderCallbacks interface).
             loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
-        }else {
+        } else {
             // Otherwise, display error
             // First, hide loading indicator so error message will be visible
             View loadingIndicator = findViewById(R.id.loading_indicator);
@@ -99,7 +106,20 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
     @Override
     public Loader<List<Location>> onCreateLoader(int id, Bundle args) {
 
-        return new EarthquakeLoader(getApplicationContext(), USGS_REQUEST_URL);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", "time");
+
+        return new EarthquakeLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -127,19 +147,21 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
 
     private static class EarthquakeLoader extends AsyncTaskLoader<List<Location>> {
 
-        /** Tag for log messages */
-        private final String LOG_TAG = EarthquakeLoader.class.getName();
+        /* Tag for log messages */
+        //private final String LOG_TAG = EarthquakeLoader.class.getName();
 
-        /** Query URL */
+        /**
+         * Query URL
+         */
         private String mUrl;
 
         /**
          * Constructs a new {@link EarthquakeLoader}.
          *
          * @param context of the activity
-         * @param url to load data from
+         * @param url     to load data from
          */
-        public EarthquakeLoader(Context context, String url) {
+        private EarthquakeLoader(Context context, String url) {
             super(context);
             mUrl = url;
         }
@@ -157,11 +179,25 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
             if (mUrl == null) {
                 return null;
             }
-
-            // Perform the network request, parse the response, and extract a list of earthquakes.
-            List<Location> earthquakes = QueryUtils. extractEarthquakes(mUrl);
-            return earthquakes;
+            return QueryUtils.extractEarthquakes(mUrl);
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
